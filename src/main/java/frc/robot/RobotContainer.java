ca4cc7df4df7bfbff8645b1c7d95c126efcd9e15
genghiskhang <khang11475@gmail.com>
2021-03-18 15:16:11 -0400

    CTRE Phoenix added

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
+//import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveWithJoysticks;
